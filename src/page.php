d7783e73597e833f482d896d963f319707a083b5
Nic Schumann <nicolas_schumann@brown.edu>
2017-10-17 09:42:11 -0400

    [Structure] stubbed base files for each custom post type.

--- src/page.php
+++ src/page.php
@@ -1,4 +1,7 @@
-<?php
+<?php get_template_part('templates/partials', 'header' ); ?>
 
+<div class="container">
+    <div class="col-sm-3"><h1>Default Page!</h1></div>
+</div>
 
-?>
+<?php get_template_part('templates/partials', 'footer' ); ?>
--- src/search.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Template Name: Search Page
- */
-
-
-?>
