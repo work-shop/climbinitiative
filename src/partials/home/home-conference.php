8ec66b13f35850e4134450af79cda1ddf8d99797
Greg Nemes <mail@gregnemes.com>
2017-10-19 13:41:55 -0400

    building out preliminary partial scaffolding

---
+++ src/partials/home/home-conference.php
@@ -0,0 +1,27 @@
+
+<section class="block spy-target vhfull" id="home-conference">
+	<div class="block-background" style="background-image: url('');">
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h1 class="white">
+					Climb<br>
+					Conference<br>
+					2017
+				</h1>
+
+			</div>
+			<div class="col-md-8">
+				<div class="border-box">
+					<h3 class="white border-box-text">
+						<?php the_field('conf_introduction','88'); ?>
+					</h3>
+					<a href="#" class="white border-box-button">
+						Conference Details
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- src/partials/home/home_intro.php
+++ src/partials/home/home_intro.php
@@ -8,13 +8,3 @@
 		</div>
 	</div>
 </section>
-
-<section class="block spy-target vhfull p5" id="home-2">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6">
-				<h1 class="white">Home 2</h1>
-			</div>
-		</div>
-	</div>
-</section>
