1f3042318c89e4de629c82aae3dc7fc02e3d213e
Greg Nemes <mail@gregnemes.com>
2017-10-19 16:28:13 -0400

    added helvetica webfonts, finished homepage intro

--- src/partials/home/home_intro.php
+++ src/partials/home/home_intro.php
@@ -1,9 +1,36 @@
 
-<section class="block spy-target vhfull p5" id="home-intro">
-	<div class="container">
+<section class="block spy-target pt7 pb7" id="home-intro">
+	<div class="container-fluid" id="home-intro-page">
 		<div class="row">
-			<div class="col-sm-6">
-				<h1 class="white">Home Intro</h1>
+			<div class="col-sm-12">
+				<div class="border-box border-box-white">
+					<div class="row mb6">
+						<div class="col-xs-12">
+							<?php get_template_part('partials/logo_full_white'); ?>
+						</div>
+					</div>
+					<div class="row mb3">
+						<div class="col-lg-10">
+							<h1 class="white">
+								<?php the_field('introduction_title') ?>
+							</h1>
+						</div>
+					</div>	
+					<div class="row mb2">
+						<div class="col-lg-10">
+							<h3 class="bold white">
+								<?php the_field('introduction_subtitle') ?>
+							</h3>
+						</div>
+					</div>	
+					<div class="row mb2">
+						<div class="col-xs-12 col-xl-10">
+							<div class="wysiwyg">
+								<?php the_field('climb_introduction') ?>
+							</div>
+						</div>
+					</div>															
+				</div>
 			</div>
 		</div>
 	</div>
