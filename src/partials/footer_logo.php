dde13ea79260bb30387f8701ab624344a2a3cab0
Greg Nemes <mail@gregnemes.com>
2017-10-19 10:31:06 -0400

    footer completed

---
+++ src/partials/footer_logo.php
@@ -0,0 +1,8 @@
+<svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 57.9 58.5" enable-background="new 0 0 57.9 58.5" xml:space="preserve">
+<g>
+	<path fill="#FFFFFF" d="M54.9,3v52.5H3V3H54.9 M57.9,0H0v58.5h57.9V0L57.9,0z"/>
+	<polygon fill="#FFFFFF" points="3,55.6 0.2,54.4 14.9,23 19.6,29 25.5,12.5 32.9,18.5 55,0.5 56.9,2.8 32.9,22.4 26.9,17.5 
+		20.6,35 15.5,28.7 	"/>
+</g>
+</svg>
--- src/partials/footer_site.php
+++ src/partials/footer_site.php
@@ -1,19 +1,34 @@
 <footer id="footer" class="block">
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
-			<div class="col-md-4 col-xs-12 footer-col">
-			
+			<div class="col-md-7 col-lg-6 col-xs-12 footer-col">
+				<div class="col-xs-4 col-sm-3 col-lg-3">
+					<?php get_template_part('partials/footer_logo'); ?>
+				</div>
+				<div class="col-xs-8 col-sm-9 col-lg-8 offset-lg-1">
+					<a href="http://equality-of-opportunity.org" target="_blank">
+						<h3 class="white">
+							CLIMB is an Initiative <br> 
+							of the Equality of<br>
+							Opportunity Project
+						</h3>
+					</a>
+				</div>
 			</div>
-			<div class="col-md-4 col-xs-12 footer-col">
-				
-			</div>
-			<div class="col-md-4 col-xs-12 footer-col">
-				
-			</div>						
+			<div class="col-md-5 col-lg-5 offset-lg-1 col-xs-12 footer-col">
+				<h4 class="white">
+					<span>
+						Climb Initiative
+					</span>
+				</h4>
+				<div class="footer-menu">
+					<?php wp_nav_menu(); ?>
+				</div>
+			</div>				
 		</div>
 		<div class="site-credit">
 			<a href="http://workshop.co" target="_blank">
-				<h4>Site by Work-Shop Design Studio</h4>
+				<h4 class="white">Site by Work-Shop Design Studio</h4>
 			</a>
 		</div>
 	</div>
